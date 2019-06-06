956d5dad09434bcc87b8670a245605b3574c9118
Andre Telfer <telfer006@gmail.com>
2019-06-06 01:01:03 -0400

    Added css-hot-loader: css hot reloading works

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 cssLoaderRule = () => {
     return {
@@ -8,6 +9,18 @@ cssLoaderRule = () => {
             camelCase: "dashes",
             modules: true,
             localIdentName: "[name]__[local]___[hash:base64:5]"
+        }
+    };
+};
+
+miniCssLoaderRule = () => {
+    return {
+        loader: MiniCssExtractPlugin.loader,
+        options: {
+            // only enable hot in development
+            hmr: process.env.NODE_ENV === "development",
+            // if hmr does not work, this is a forceful method.
+            reloadAll: true
         }
     };
 };
@@ -23,15 +36,27 @@ const config = {
             },
             {
                 test: /\.css$/,
-                use: ["style-loader", cssLoaderRule()],
+                use: ["css-hot-loader", miniCssLoaderRule(), cssLoaderRule()],
                 exclude: /\.module\.css$/
             },
+
             {
                 test: /\.less$/,
-                use: ["style-loader", cssLoaderRule(), "less-loader"]
+                use: [
+                    "css-hot-loader",
+                    miniCssLoaderRule(),
+                    cssLoaderRule(),
+                    "less-loader"
+                ]
             }
         ]
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
