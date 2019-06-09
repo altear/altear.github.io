6418967918ad37f8d69c4f06b7aa760c5335d059
Andre Telfer <telfer006@gmail.com>
2019-06-09 17:44:47 -0400

    Building website- experimenting with CSS3 animations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import styles from "./styles.less";
 
 var el = document.createElement("h1");
-el.innerHTML = "Hello World123";
+el.innerHTML = "Hello World";
 el.className = styles.myClass;
-document.body.appendChild(el);
+// document.body.appendChild(el);
 
-console.log("Hello world");
+// console.log("Hello world");
