373055637e9e18a18afa270e7f925800df86d4f9
Andre Telfer <telfer006@gmail.com>
2019-06-10 07:31:37 -0400

    Added SVG animation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import styles from "./styles.less";
-
-var el = document.createElement("h1");
-el.innerHTML = "Hello World";
-el.className = styles.myClass;
-// document.body.appendChild(el);
-
-// console.log("Hello world");
